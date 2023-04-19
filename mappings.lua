12785868daa8ff1455c2f60d647623f86ebcc6cc
bernwo <53580103+bernwo@users.noreply.github.com>
2023-04-19 23:18:32 +0800

    Add new mappings in `mappings.lua`

--- mappings.lua
+++ mappings.lua
@@ -19,7 +19,8 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>E"] = { ":e!<cr>", desc = "Reset File" },
   },
   t = {
     -- setting a mapping to false will disable it
